fafd6bb03192a32d42a159bb88920c2931aa86a2
mr-nsin <singhal.nitin94@gmail.com>
2022-09-26 15:44:33 +0530

    Part#21 -> Inheritance Example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+FullTimeEmployee FTE = new FullTimeEmployee();
+FTE.Firstname = "Nitin";
+FTE.Lastname = "Singhal";
+FTE.Email = "singhal.nitin108@yahoo.com";
+FTE.YearlySalary = 500000;
+FTE.PrintFullName();
+
+public class Employee
+{
+    public string Firstname;
+    public string Lastname;
+    public string Email;
+
+    public void PrintFullName()
+    {
+        Console.WriteLine(Firstname + " " + Lastname);
+        }
+}
+
+public class FullTimeEmployee : Employee
+{
+    public float YearlySalary;
+}
+
+public class PartTimeEmployee : Employee
+{
+    public float HourlyRate;
+}
+
