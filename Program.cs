03b967481b36e719cca6e808d1c733c7cac8a3fc
mr-nsin <singhal.nitin94@gmail.com>
2022-09-27 10:30:51 +0530

    Part#24 -> Method Overloading example, override keyword, Base class reference variable call dervied class method at run time.

--- Program.cs
+++ Program.cs
@@ -1,53 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-Employee[] employees = new Employee[4];
+BaseClass b = new DerivedClass();
+b.Print();
 
-employees[0] = new Employee();
-employees[1] = new PartTimeEmployee();
-employees[2] = new FullTimeEmployee();
-employees[3] = new TemporaryEmployee();
-
-
-foreach (Employee employee in employees)
+public class BaseClass
 {
-    employee.PrintFullName();
-}
-
-
-public class Employee
-{
-    public string Firstname = "FN";
-    public string Lastname = "LN";      
-
-    public virtual void PrintFullName()
+    public virtual void Print()
     {
-        Console.WriteLine(Firstname + " " + Lastname);
+        Console.WriteLine("I am a Base Class Print Method");
     }
 }
 
-public class FullTimeEmployee : Employee
+public class DerivedClass : BaseClass
 {
-    public override void PrintFullName()
+    public override void Print()
     {
-        Console.WriteLine(Firstname + " " + Lastname + " - Full Time");
-    }
-    
-}
-
-public class PartTimeEmployee : Employee
-{
-    public override void PrintFullName()
-    {
-        Console.WriteLine(Firstname + " " + Lastname + " - Part Time");
-    }
-}
-
-public class TemporaryEmployee : Employee
-{
-    public override void PrintFullName()
-    {
-        Console.WriteLine(Firstname + " " + Lastname + " - Temporary");
+        Console.WriteLine("I am derived class print method");
     }
 }
 
