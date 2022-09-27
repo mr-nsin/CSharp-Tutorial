d81367b580eb164a03e2b81dcd902066d849ae2c
mr-nsin <singhal.nitin94@gmail.com>
2022-09-27 19:58:04 +0530

    Part#26

--- Program.cs
+++ Program.cs
@@ -1,22 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-BaseClass b = new DerivedClass();
-b.Print();
 
-public class BaseClass
+Student C1 = new Student();
+C1.SetID(101);
+C1.SetName(null);
+
+public class Student
 {
-    public virtual void Print()
+    private int _id;
+    private string _Name;
+    private int _passMark = 35;
+
+    public void SetName(string Name)
     {
-        Console.WriteLine("I am a Base Class Print Method");
+        if (string.IsNullOrEmpty(Name))
+        {
+            throw new Exception("Student name can't be null or empty");
+        }
+        this._Name = Name;
     }
-}
 
-public class DerivedClass : BaseClass
-{
-    public new void Print()
+    public string GetName()
     {
-        Console.WriteLine("I am derived class print method");
+        if (string.IsNullOrEmpty(this._Name))
+        {
+            return "No Name";
+        }
+        else
+        {
+            return this._Name;
+        }
+    }
+
+
+    public void SetID(int Id)
+    {
+        if (Id <= 0)
+        {
+            throw new Exception("Student ID can not be negative and zero");
+        }
+        this._id = Id;
+    }
+
+    public int GetID()
+    {
+        return this._id;
     }
 }
 
